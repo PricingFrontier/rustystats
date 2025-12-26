3f94f41725fba563fe65f948dbe9739727252506
Ralph <ralph@pricing-frontier.co.uk>
2025-12-26 16:39:20 +0000

    Deployed abef6dd with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,25 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      boldsymbol: ["\\mathbf{#1}", 1]
+    }
+  },
+  loader: {
+    load: ['[tex]/boldsymbol']
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
